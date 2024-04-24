7d461120fc325d4a759895e0ffc50caac6e69ca5
Bruno Paulino <bruno@bpaulino.com>
2024-04-24 08:06:38 +0200

    Initial Commit

---
+++ clox_xcode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  clox_xcode
+//
+//  Created by Bruno Paulino on 24.04.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
